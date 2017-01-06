09096689b1c3641d2184f007a78354c3211f0ede
ZakFahey <z.fahey@gmail.com>
2017-01-06 14:44:49 -0500

    Version tick

--- Plugin.cs
+++ Plugin.cs
@@ -62,7 +62,7 @@ namespace SmartRegions
         }
         public override Version Version
         {
-            get { return new Version("1.0"); }
+            get { return new Version("1.1"); }
         }
         public override string Name
         {
