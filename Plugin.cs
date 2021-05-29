8cfbdb52dd62ae1e186d331c169e54b6dff151ba
ZakFahey <zfahey@google.com>
2021-05-29 09:04:53 -0700

    Update version tick

--- Plugin.cs
+++ Plugin.cs
@@ -62,7 +62,7 @@ namespace SmartRegions
         }
         public override Version Version
         {
-            get { return new Version("1.2.1"); }
+            get { return new Version("1.3.0"); }
         }
         public override string Name
         {
